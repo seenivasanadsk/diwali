2836145bce8866421a51a8e5113d5ce36b7dac44
Seenivasan <seenivasanadsk44@gmail.com>
2021-11-02 21:01:40 +0530

    Project Finished

--- index.js
+++ index.js
@@ -1,48 +1,81 @@
 window.onload = () => {
 
-    let body = document.querySelector('body')
+    let seeni = () => {
+        let body = document.querySelector('body')
 
-    let element = (tag, arg, child) => {
-        let element = document.createElement(tag);
-        element.innerHTML = child ? child : "";
-        if (arg) {
-            for (i in arg) element.setAttribute(i, arg[i])
+        let element = (tag, arg, child) => {
+            let element = document.createElement(tag);
+            element.innerHTML = child ? child : "";
+            if (arg) {
+                for (i in arg) element.setAttribute(i, arg[i])
+            }
+            return element
         }
-        return element
-    }
 
-    let sendroc = element("div", { class: "sendroc" })
-    let spark = element("div", { class: "spark" })
-    let firework = element("div", { class: "firework" })
-    let rocket = element('div', { class: "rocket" })
+        let sendroc = element("div", { class: "sendroc" })
+        let spark = element("div", { class: "spark" })
+        let firework = element("div", { class: "firework" })
+        let rocket = element('div', { class: "rocket" })
 
-    for (let i = 0; i < 20; i++) {
-        let org = spark.cloneNode()
-        org.style.transform = `rotate(${18 * i}deg)`;
-        firework.appendChild(org);
-    }
+        let sparkWidth,sparkHeight = undefined
 
-    sendroc.appendChild(firework);
-    sendroc.appendChild(rocket);
-    body.appendChild(sendroc);
-
-    rocket.style.left = `${Math.floor(Math.random()*100)}%`
-    rocket.onanimationend = e => {
-        let fire = firework
-        colors = ["red",'yellow','lime','magenta','aqua','orange','white']
-        fire.style.background = colors[Math.floor(Math.random()*7)]
-        rocPos = e.target.getBoundingClientRect();
-        rocX = rocPos.x
-        rocY = rocPos.y
-        fire.style.top = rocY
-        fire.style.left = rocX
-        fire.style.display = "block";
-        fire.onanimationend = e => {
-            rocket.classList.remove('rocket');
-            rocket.classList.add('rocket');
-            rocket.style.display = "block"
-            fire.style.display = "none"            
+        if(window.innerWidth < window.innerHeight){
+            sparkWidth = ".3vw"
+            sparkHeight = "20vw"
+        }else {
+            sparkWidth = ".3vh"
+            sparkHeight = "20vh"
         }
-        e.target.style.display = "none";
+
+        for (let i = 0; i < 30; i++) {
+            let org = spark.cloneNode()
+            org.style.transform = `rotate(${12 * i}deg)`;
+            org.style.width = sparkWidth
+            org.style.height = sparkHeight
+            firework.appendChild(org);
+        }
+
+        rocket.onanimationend = e => {
+            let fire = firework;
+            let colors = ["red", 'yellow', 'lime', 'magenta', 'aqua', 'orange', 'white']
+            fire.style.background = colors[Math.floor(Math.random() * 7)]
+            rocPos = e.target.getBoundingClientRect();
+            rocX = rocPos.x
+            rocY = rocPos.y
+            fire.style.top = rocY
+            fire.style.left = rocX
+            fire.style.display = "block";
+            let leftPos = ['20%', '40%', '60%', '80%']
+            let topPos = ['25%',"50%",'38%']
+            let aniDly = ['1s',".5s","2s","1.5s"]
+            rocket.style.left = leftPos[Math.floor(Math.random() * 4)]
+            rocket.style.top = topPos[Math.floor(Math.random() * 3)]
+            rocket.style.animationDelay = aniDly[Math.floor(Math.random() * 4)]
+            fire.onanimationend = e => {
+                rocket.classList.remove('rocket');
+                rocket.classList.add('rocket');
+                rocket.style.display = "block"
+                rocket.style.visibility = "hidden"
+                fire.style.display = "none"
+            }
+            e.target.style.display = "none";
+        }
+        rocket.onanimationstart = () => rocket.style.visibility = "visible"
+
+        sendroc.appendChild(firework);
+        sendroc.appendChild(rocket);
+        body.appendChild(sendroc);
     }
+
+    seeni();
+    seeni();
+    seeni();
+    seeni();
+    seeni();
+    seeni();
+    seeni();
+    seeni();
+    seeni();
+    seeni();
+
 }
