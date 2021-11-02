07e7134a03c45919348a8a6b7a5c64d1e9c9f245
Seenivasan <seenivasanadsk44@gmail.com>
2021-11-02 14:23:03 +0530

    remove console log in javascipt

--- index.js
+++ index.js
@@ -26,15 +26,8 @@ window.onload = () => {
     sendroc.appendChild(rocket);
     body.appendChild(sendroc);
 
-    // for (let i = 0; i < 20; i++) {
-    //     let e = document.querySelector(`.firework .spark:nth-child(${i + 1})`)
-    //     e.style.transform = `rotate(${18 * i}deg)`;
-    // }
-    // let rocket = document.querySelector('.rocket');
     rocket.style.left = `${Math.floor(Math.random()*100)}%`
     rocket.onanimationend = e => {
-        console.log('rocket and')
-        // let fire = document.querySelector('.firework')
         let fire = firework
         colors = ["red",'yellow','lime','magenta','aqua','orange','white']
         fire.style.background = colors[Math.floor(Math.random()*7)]
@@ -45,13 +38,11 @@ window.onload = () => {
         fire.style.left = rocX
         fire.style.display = "block";
         fire.onanimationend = e => {
-            console.log('firework and')
             rocket.classList.remove('rocket');
             rocket.classList.add('rocket');
             rocket.style.display = "block"
             fire.style.display = "none"            
         }
-        console.log(rocX, rocY);
         e.target.style.display = "none";
     }
 }
